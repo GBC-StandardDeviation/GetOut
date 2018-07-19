d11406d0c1910f39ae42f5f8f2a325207536c9a8
NikolaKordic <39223710+NikolaKordic@users.noreply.github.com>
2018-07-19 19:54:16 -0400

    AssetUpdate
    AssetUpdate

--- javascript/main-alpha.js
+++ javascript/main-alpha.js
@@ -143,28 +143,28 @@ function loop() {
         //misteryItem: 515098
     };
     var chestLevel1 = {
-        img: "url('images/b_table.png')",
+        img: "url('images/table.png')",
         dial: "You reached a chest"
         // + "<br>" + "There is a bat on the chest"
     };
     var keyItem = "Key";
     var lockerLevel1 = {
-        img: "url('images/cuboard.png')",
+        img: "url('images/universal_locker2.jpg')",
         dial: "You reached a locker" + "<br>" + "Please enter 4 digit number to open",
         invtory: ["To Headmaster of University College London", keyItem],
         misteryItem: 2012
     };
     var bookShelfLevel1 = {
-        img: "url('images/shelf.png')",
+        img: "url('images/bookshelf.png')",
         dial: "You reached a bookShelf",
         invtory: ["Math Book", "History Book", "Geography Book", "Science Book"]
     };
     var dresserLevel1 = {
-        img: "url('images/dresser.png')",
+        img: "url('images/dresser2.png')",
         dial: "You reached a Dresser"
     };
     var smallTableLevel1 = {
-        img: "url('images/small_table.png')",
+        img: "url('images/table2.png')",
         dial: "You reached a small table"
     };
     var rugLevel1 = {
@@ -172,7 +172,7 @@ function loop() {
         dial: "You reached a rug"
     };
     var bedLevel1 = {
-        img: "url('images/bed.png')",
+        img: "url('images/single_bed_1-50.png')",
         dial: "You reached a bed"
     };
     //Game object declaration and initialization at level one --- End;
@@ -198,13 +198,13 @@ function loop() {
     floor.src = "images/floor_game2.jpg";
     floor.onload = imageLoader;
     var dresser = new Image();
-    dresser.src = "images/dresser.png";
+    dresser.src = "images/dresser2.png";
     dresser.onload = imageLoader;
     var s_table = new Image();
-    s_table.src = "images/small_table.png";
+    s_table.src = "images/table2.png";
     s_table.onload = imageLoader;
     var b_table = new Image();
-    b_table.src = "images/b_table.png";
+    b_table.src = "images/table.png";
     b_table.onload = imageLoader;
     var s_window = new Image();
     s_window.src = "images/window.png";
@@ -217,7 +217,7 @@ function loop() {
     s_shelf.src = "images/bookshelf.png";
     s_shelf.onload = imageLoader;
     var s_bed = new Image();
-    s_bed.src = "images/bed.png";
+    s_bed.src = "images/single_bed_1-50.png";
     s_bed.onload = imageLoader;
     var l_rug = new Image();
     l_rug.src = "images/rug.png";
